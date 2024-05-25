f85b5dd8b849599b6d59158deb142a4e93b9855e
gery-guedegbe <guedegbe2003@gmail.com>
2024-05-25 21:16:45 +0100

    Third Commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
 
